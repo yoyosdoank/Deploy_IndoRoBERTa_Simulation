95cf5a33b3ee76643b1b64bb68c8f2bbe2b97fbe
yoyosdoank <yogie.sihombing27@gmail.com>
2024-05-25 18:11:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ def get_model():
 
 tokenizer,model1,model2 = get_model()
 
+header = st.header("Klasifikasi Sentimen dan Emosi Berbasis Teks Pada Twitter Berbahasa Indonesia Dengan Model LLM (Large Language Model)")
 user_input = st.text_area('Masukkan postingan/komentar Twitter yang akan dianalisis:')
 note1 = st.caption("*Untuk masukan kalimat, direkomendasikan lebih dari 7 kata, agar mesin dapat merepresentasi konteks kalimat.")
 note2 = st.caption("*Rekomendasi analisis: Twitter.")
