f2c73552c7cd19855d4bf92a2d31d48d8a7afd01
yoyosdoank <yogie.sihombing27@gmail.com>
2024-05-25 16:40:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ tokenizer,model1,model2 = get_model()
 
 user_input = st.text_area('Masukkan postingan/komentar Twitter yang akan dianalisis:')
 note1 = st.caption("*Note: Untuk masukan kalimat, direkomendasikan lebih dari 7 kata, agar mesin dapat merepresentasi konteks kalimat.")
-note2 = st.caption("*Dimungkinkan analisis dari postingan media sosial lainnya")
+note2 = st.caption("*Dimungkinkan analisis dari media sosial lainnya")
 button = st.button("Lakukan Analisis")
 
 d = {
