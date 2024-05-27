b38805abcbb882b05340f1a380dabe337c3ec6c8
yoyosdoank <yogie.sihombing27@gmail.com>
2024-05-28 02:58:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,14 +23,14 @@ st.image("banner_edit.png", use_column_width=True)
 header = st.title("Prediksi Sentimen & Emosi Pada Kalimat Berbahasa Indonesia Dengan Metode Transformers")
 note1 = st.caption("**AUTHOR: YOGIE OKTAVIANUS SIHOMBING**")
 note2 = st.write("SENTIMEN adalah sikap, perasaan, atau pandangan yang lebih stabil dan cenderung bertahan lebih lama terhadap seseorang, situasi, atau fenomena tertentu. Sentimen merupakan cerminan dari emosi yang lebih menetap dan terinternalisasi. EMOSI adalah respons psikologis yang intens, sering kali singkat, terhadap suatu peristiwa atau situasi. Emosi biasanya bersifat sementara dan bisa berubah dengan cepat. ***- Ivanov, D. (2023) -***")
-st.info("*Info: Masukkan kalimat Anda di kolom bawah dan tekan 'Lakukan Analisis' untuk memulai.")
+st.info("*Info: Masukkan kalimat Anda di kolom bawah dan tekan 'ANALISIS' untuk memulai prediksi. Tombol 'RESET' untuk refresh halaman.")
 user_input = st.text_area('Inputkan Kalimat:')
 note3 = st.caption("****Harap memasukkan kalimat yang mempunyai konteks, minimal 7 kata dalam 1 kalimat.***")
 note4 = st.caption("****Rekomendasi media sosial berbasis teks: Twitter.***")
 note5 = st.caption("****Dimungkinkan analisis dari media sosial lainnya.***")
 note6 = st.caption("****Analisis selain menggunakan bahasa Indonesia tidak dibenarkan.***")
-button = st.button("Lakukan Analisis")
-reset_button = st.button("Reset")
+button = st.button("ANALISIS")
+reset_button = st.button("RESET")
 
 sentimen = {
   2:'Positif',  
