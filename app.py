ddb0778e797a7c254fde3f55bcfd6bf63491baa7
yoyosdoank <yogie.sihombing27@gmail.com>
2024-05-28 02:13:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def get_model():
 
 tokenizer,model1,model2 = get_model()
 
-header = st.header("Prediksi Sentimen & Emosi Pada Kalimat Berbahasa Indonesia Dengan Metode Transformers")
+header = st.title("Prediksi Sentimen & Emosi Pada Kalimat Berbahasa Indonesia Dengan Metode Transformers")
 note1 = st.caption("**AUTHOR: YOGIE OKTAVIANUS SIHOMBING**")
 note2 = st.caption("SENTIMEN adalah sikap, perasaan, atau pandangan yang lebih stabil dan cenderung bertahan lebih lama terhadap seseorang, situasi, atau fenomena tertentu. Sentimen merupakan cerminan dari emosi yang lebih menetap dan terinternalisasi. EMOSI adalah respons psikologis yang intens, sering kali singkat, terhadap suatu peristiwa atau situasi. Emosi biasanya bersifat sementara dan bisa berubah dengan cepat. ***-Ivanov, D. (2023)-.***")
 st.info("*Info: Masukkan kalimat Anda di kolom bawah dan tekan 'Lakukan Analisis' untuk memulai.")
