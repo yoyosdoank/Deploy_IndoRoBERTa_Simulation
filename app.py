6b2ca022afb767f109d9fe0e851baa0dfd6e50bb
yoyosdoank <yogie.sihombing27@gmail.com>
2024-05-28 11:02:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -133,8 +133,8 @@ st.markdown("""
         <li><strong>Rekomendasi media sosial berbasis teks: Twitter.</strong></li>
         <li><strong>Dimungkinkan analisis dari media sosial lainnya.</strong></li>
         <li><strong>Analisis selain menggunakan bahasa Indonesia tidak disarankan.</strong></li>
-        <li><strong>Warning yang muncul saat analisis, hanya sebagai reminder.</strong></li>
-        <li><strong>Jika muncul 5 atau lebih Warning, prediksi tidak akan dijalankan, harap kalimat diperbaiki.</strong></li>
+        <li><strong>Peringatan yang muncul saat analisis, hanya sebagai reminder.</strong></li>
+        <li><strong>Jika muncul 5 atau lebih peringatan, prediksi tidak dijalankan, harap kalimat diperbaiki.</strong></li>
     </ol>
 </div>
 """, unsafe_allow_html=True)
