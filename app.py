c8b7ffdf245679d770208128a0d7e13879f72598
yoyosdoank <yogie.sihombing27@gmail.com>
2024-05-30 00:16:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -174,7 +174,7 @@ st.markdown("""
         <li><strong>Dimungkinkan analisis dari media sosial lainnya.</strong></li>
         <li><strong>Analisis selain menggunakan bahasa Indonesia tidak disarankan.</strong></li>
         <li><strong>Peringatan berwarna <span style="color: yellow;">KUNING</span> muncul saat analisis sebagai reminder.</strong></li>
-        <li><strong>jika peringatan muncul < 7, kalimat tetap dapat diprediksi. Jika peringatan muncul > 7, prediksi tidak berjalan.</strong></li>
+        <li><strong>jika <span style="color: yellow;">peringatan</span> muncul < 7, kalimat tetap dapat diprediksi. Jika <span style="color: yellow;">peringatan</span> muncul > 7, prediksi tidak berjalan.</strong></li>
         <li><strong>Untuk diketahui, hasil prediksi perlu dipertimbangkan kembali dari ahlinya, khususnya kategori 'MODERAT' dan 'KURANG'. </strong></li>
     </ol>
 """, unsafe_allow_html=True)
