7cf277fb661a8c2b71c9a28e4dd4a852c51ff89f
yoyosdoank <yogie.sihombing27@gmail.com>
2024-05-29 10:53:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def get_model():
     model1 = AutoModelForSequenceClassification.from_pretrained("yogie27/IndoRoBERTa-Sentiment-Classifier-for-Twitter", token="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")
     model2 = AutoModelForSequenceClassification.from_pretrained("yogie27/IndoRoBERTa-Emotion-Classifier-Base", token="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")
     model3 = AutoModelForSequenceClassification.from_pretrained("yogie27/IndoRoBERTa-Hatespeech-Classifier-Base", token="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")
-    return tokenizer, model1, model2
+    return tokenizer, model1, model2, model3
 
 tokenizer, model1, model2, model3 = get_model()
 
